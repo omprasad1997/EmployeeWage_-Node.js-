768259e372bbb2b0446639069800f3b75bd85cc4
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-03-23 17:21:49 +0530

    Updated:Calculate Wages till a condtion of total working hours of 160 or max daysof 20 is reached for month

--- EmpWage.js
+++ EmpWage.js
@@ -3,7 +3,18 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR   = 20;
-const NUM_OF_WORKING_DAYS = 2;
+const MAX_HRS_IN_MONTH = 100;
+const NUM_OF_WORKING_DAYS = 10;
+
+let totalEmpHrs = 0;
+let totalWorkingDays=0;
+
+while(totalEmpHrs <= MAX_HRS_IN_MONTH &&
+	totalWorkingDays < NUM_OF_WORKING_DAYS){
+      totalWorkingDays++;
+      let  empCheck = Math.floor(Math.random()*10)%3;
+      totalEmpHrs += getWorkingHours(empCheck);
+}
 
 function getWorkingHours(empCheck){
    switch(empCheck){
@@ -16,12 +27,7 @@ function getWorkingHours(empCheck){
   }
 }
 
-let empHrs = 0;
-for(let day = 0;day<NUM_OF_WORKING_DAYS;day++){
-	let  empCheck = Math.floor(Math.random()*10)%3;
-	empHrs += getWorkingHours(empCheck);
-}
-
-let empWage = empHrs*WAGE_PER_HOUR;
-console.log("Total Hrs:"+empHrs+" Emp Wage:" + empWage);
+let empWage = totalEmpHrs*WAGE_PER_HOUR;
+console.log("Total Days:"+totalWorkingDays+
+" Total Hrs:" + totalEmpHrs + " Emp Wage:" + empWage);
 
