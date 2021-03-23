a9cef290aae8ffceff8fea68e04b4b4ea8cb9444
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-03-23 16:29:06 +0530

    Refactored : Refactor the code to write a function to get work hours

--- EmpWage.js
+++ EmpWage.js
@@ -8,7 +8,7 @@ function getWorkingHours(empCheck){
    switch(empCheck){
      case IS_PART_TIME:
 	return PART_TIME_HOURS;
-     case IS_PART_TIME:
+     case IS_FULL_TIME:
         return FULL_TIME_HOURS;
      default:
         return 0;
