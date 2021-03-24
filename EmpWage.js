38e1f88f086bb12e3e8801c60ee90c0af2dd6759
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-03-24 21:22:52 +0530

    Updated:Store the daily wage along with the total wage

--- EmpWage.js
+++ EmpWage.js
@@ -3,17 +3,24 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR   = 20;
-const MAX_HRS_IN_MONTH = 100;
-const NUM_OF_WORKING_DAYS = 10;
+const MAX_HRS_IN_MONTH = 160;
+const NUM_OF_WORKING_DAYS = 20;
 
 let totalEmpHrs = 0;
 let totalWorkingDays=0;
+let empDailyWageArr = new Array();
 
 while(totalEmpHrs <= MAX_HRS_IN_MONTH &&
 	totalWorkingDays < NUM_OF_WORKING_DAYS){
       totalWorkingDays++;
       let  empCheck = Math.floor(Math.random()*10)%3;
-      totalEmpHrs += getWorkingHours(empCheck);
+      let empHrs    = getWorkingHours(empCheck);
+      totalEmpHrs += empHrs;
+      empDailyWageArr.push(calcDailyWage(empHrs));
+}
+
+function calcDailyWage(empHrs){
+   return empHrs*WAGE_PER_HOUR;
 }
 
 function getWorkingHours(empCheck){
@@ -27,7 +34,7 @@ function getWorkingHours(empCheck){
   }
 }
 
-let empWage = totalEmpHrs*WAGE_PER_HOUR;
+let empWage = calcDailyWage(totalEmpHrs);
 console.log("Total Days:"+totalWorkingDays+
 " Total Hrs:" + totalEmpHrs + " Emp Wage:" + empWage);
 
