af20f2ee032567c4f8ed354dc4bf22a0603a30ec
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-03-25 17:13:44 +0530

    Updated : Ability to store the day,hours worked and wage earned in single object

--- EmpWage.js
+++ EmpWage.js
@@ -17,16 +17,13 @@ function getWorkingHours(empCheck){
       }
    }
 
+function calcDailyWage(empHrs){
+      return empHrs*WAGE_PER_HOUR;
+}
+
 let totalEmpHrs = 0;
 let totalWorkingDays=0;
-let empDailyWageArr = new Array();
-let empDailyWageMap = new Map();
-let empDailyHrsMap = new Map();
-
-
-function calcDailyWage(empHrs){
-   return empHrs*WAGE_PER_HOUR;
-}
+let empDailyHrsAndWageArr = new Array();
 
 while(totalEmpHrs <= MAX_HRS_IN_MONTH &&
 	totalWorkingDays < NUM_OF_WORKING_DAYS){
@@ -34,44 +31,16 @@ while(totalEmpHrs <= MAX_HRS_IN_MONTH &&
       let  empCheck = Math.floor(Math.random()*10)%3;
       let empHrs    = getWorkingHours(empCheck);
       totalEmpHrs += empHrs;
-      empDailyWageArr.push(calcDailyWage(empHrs));
-      empDailyHrsMap.set(totalWorkingDays,empHrs);
-      empDailyWageMap.set(totalWorkingDays,calcDailyWage(empHrs));
+      empDailyHrsAndWageArr.push({
+         dayNum:totalWorkingDays,
+         dailyHours:empHrs,
+         dailyWage:calcDailyWage(empHrs),
+         toString(){
+            return'\nDay' + this.dayNum + '=> Working Hours is ' + this.dailyHours +
+                        ' And Wage Earned = ' + this.dailyWage
+         },
+         
+       });
 }
+console.log("UC 10 Showing Daily Hours Worked and Wage Earned: "+ empDailyHrsAndWageArr);
 
-console.log(empDailyWageMap);
-function totalWages(totalWage,dailyWage){
-   return totalWage + dailyWage;
-}
-
-console.log("UC8 - Emp Wage Map totalHrs: " + 
-               Array.from(empDailyWageMap.values()).reduce(totalWages,0));
-
-
-
-const findTotal = (totalVal,dailyVal) => {
-   return totalVal + dailyVal;
-}
-
-let totalHours = Array.from(empDailyHrsMap.values())
-                  .filter(dailyHour => dailyHour>0)
-                  .reduce(findTotal,0);
-let totalSalary = empDailyWageArr.filter(dailyWage=>dailyWage>0)
-                  .reduce(findTotal,0);
-
-
-console.log("UC9A - Emp Wage with arrow.: " + " Total Hours: "+
-                  totalHours + " Total Wage: " + totalSalary);
-
-let nonWorkingDays = new Array();
-let partWorkingDays = new Array();
-let fullWorkingDays = new Array();
-empDailyHrsMap.forEach((value,key) =>{
-   if(value == 8) fullWorkingDays.push(key);
-   else if(value == 4) partWorkingDays.push(key);
-   else nonWorkingDays.push(key);
-});
-
-console.log('Full Working Days: '+fullWorkingDays);
-console.log("Part Working Days: "+partWorkingDays);
-console.log("Non Working Days: " +nonWorkingDays);
