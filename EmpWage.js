777f72e5043534e71691a7c7fcd03b5cab59150b
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-03-25 18:13:36 +0530

    Refacored : Abilty to calculate the day ,hours worked and wage earned using arrow function

--- EmpWage.js
+++ EmpWage.js
@@ -21,6 +21,7 @@ function calcDailyWage(empHrs){
       return empHrs*WAGE_PER_HOUR;
 }
 
+
 let totalEmpHrs = 0;
 let totalWorkingDays=0;
 let empDailyHrsAndWageArr = new Array();
@@ -44,3 +45,25 @@ while(totalEmpHrs <= MAX_HRS_IN_MONTH &&
 }
 console.log("UC 10 Showing Daily Hours Worked and Wage Earned: "+ empDailyHrsAndWageArr);
 
+let totalWages = empDailyHrsAndWageArr
+                 .filter(dailyHrsAndWage => dailyHrsAndWage.dailyWage > 0)
+                 .reduce((totalWage,dailyHrsAndWage) => totalWage += dailyHrsAndWage.dailyWage,0);
+let totalHours = empDailyHrsAndWageArr
+                 .filter(dailyHrsAndWage => dailyHrsAndWage.dailyWage > 0)
+                 .reduce((totalHours,dailyHrsAndWage) => totalHours += dailyHrsAndWage.dailyHours,0);
+console.log("UC 11A Total Hours: " + totalHours + 'Total Wages: '+totalWages);
+
+process.stdout.write("UC 11B Logging full work days")
+empDailyHrsAndWageArr.filter(dailyHrsAndWage => dailyHrsAndWage.dailyHours==8)
+                     .forEach(dailyHrsAndWage => process.stdout.write(dailyHrsAndWage.toString()));
+
+let partWorkingDayStrArr = empDailyHrsAndWageArr
+                           .filter(dailyHrsAndWage => dailyHrsAndWage.dailyHours == 4)
+                           .map(dailyHrsAndWage => dailyHrsAndWage.toString());
+console.log("\nUC 11C PartWorkingDayStrings: "+partWorkingDayStrArr);
+
+let nonWorkingDayNums = empDailyHrsAndWageArr
+                        .filter(dailyHrsAndWage => dailyHrsAndWage.dailyHours == 0)
+                        .map(dailyHrsAndWage => dailyHrsAndWage.dayNum);
+
+console.log("UC 11D NonWorkingDaysNums: "+ nonWorkingDayNums);
